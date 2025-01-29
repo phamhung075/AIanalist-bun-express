4c27b944cb02b2ed30b0a838c4a740d76ceb1438
Dai Hung PHAM <daihung.pham@gmail.com>
2025-01-29 02:28:41 +0100

    fix bug notfound

--- src/_core/middleware/errorHandler.ts
+++ src/_core/middleware/errorHandler.ts
@@ -4,43 +4,56 @@ import _ERROR from '../helper/http-status/error/index';
 import { logResponse } from '../helper/http-status/response-log';
 
 // Not Found Handler Middleware
-export const notFoundMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  next(new _ERROR.NotFoundError({
-    message: 'The requested resource was not found.',
-    status: HttpStatusCode.NOT_FOUND
-  }));
+export const notFoundMiddleware = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	console.log('✅ notFound ------------');
+
+	next(
+		new _ERROR.NotFoundError({
+			message: 'The requested resource was not found.',
+			status: HttpStatusCode.NOT_FOUND,
+		})
+	);
 };
 
 // Error Handler Middleware
-export const errorHandlerMiddleware = (error: any, req: Request, res: Response, next: NextFunction) => {
-  console.error(error)
-  logResponse(req, JSON.stringify(error, null, 2));
+export const errorHandlerMiddleware = (
+	error: any,
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	console.error(error);
+	logResponse(req, JSON.stringify(error, null, 2));
 
-  // Already sent response
-  if (res.headersSent) {
-    return next(error);
-  }
+	// Already sent response
+	if (res.headersSent) {
+		return next(error);
+	}
 
-  // Get error status and message
-  const status = error.status || HttpStatusCode.INTERNAL_SERVER_ERROR;
-  const message = error.message || 'Internal Server Error';
-  console.log('errorHandlerMiddleware', status);
-  // Create appropriate error instance
-  let errorResponse;
-  if (error instanceof _ERROR.ErrorResponse) {
-    errorResponse = error;
-  } else if (status === HttpStatusCode.NOT_FOUND) {
-    errorResponse = new _ERROR.NotFoundError({ message });
-  } else {
-    errorResponse = new _ERROR.InternalServerError({ message });
-  }
+	// Get error status and message
+	const status = error.status || HttpStatusCode.INTERNAL_SERVER_ERROR;
+	const message = error.message || 'Internal Server Error';
+	console.log('errorHandlerMiddleware', status);
+	// Create appropriate error instance
+	let errorResponse;
+	if (error instanceof _ERROR.ErrorResponse) {
+		errorResponse = error;
+	} else if (status === HttpStatusCode.NOT_FOUND) {
+		errorResponse = new _ERROR.NotFoundError({ message });
+	} else {
+		errorResponse = new _ERROR.InternalServerError({ message });
+	}
 
-  // Send error response
-  return errorResponse.send(res);
+	// Send error response
+	return errorResponse.send(res);
 };
 
 // Export both middlewares
 export const errorMiddleware = {
-  notFound: notFoundMiddleware,
-  errorHandler: errorHandlerMiddleware
+	notFound: notFoundMiddleware,
+	errorHandler: errorHandlerMiddleware,
 };
